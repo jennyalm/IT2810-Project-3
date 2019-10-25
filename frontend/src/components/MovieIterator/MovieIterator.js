7a91e58e153b99f17a433e667edcaf50a78d2f01
haakofli <hakon.fuhre.lia@gmail.com>
2019-10-25 21:52:58 +0200

    #5

--- frontend/src/components/MovieIterator/MovieIterator.js
+++ frontend/src/components/MovieIterator/MovieIterator.js
@@ -1,37 +1,52 @@
 import React, { useState } from 'react'
 import Movie from '../Movie/Movie'
 import './MovieIterator.css';
-import PopUp from "../PopUp/PopUp.js";
-
-
+import Modal, {closeStyle} from 'simple-react-modal'
+import {Button} from 'reactstrap'
 
 function MovieIterator(props){
 
 
-    const [showPopup, setShowPopup] = useState(false);
+    const [show, setShow] = useState(false);
 
+    const [title, setTitle] = useState("");
+    const [plot, setPlot] = useState("");
+    const [genre, setGenre] = useState("")
 
     // added keys to every child component to get rid of a error in the console.
+    
+    const callPopup = (t, p, g) => {
+        setShow(!show)
+        setTitle(t);
+        setPlot(p);
+        setGenre(g)
+    }
+    
+    const myStyle = {
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+        height: "100%",
+        width: "100%"
+      };
+    const containerStyle = {
+        background: "black",
+        width: "50%",
+        height: "70%",
+        textAlign: "center",
+        display: "flex",
+        alignContent: "center"
+    }
 
     const displayMovies = (props.movies)
         .map((movie, index) => (
                 <div key={movie._id} >
                     <Movie 
-                        onClick={() => setShowPopup(true)} 
+                        onClick={() => callPopup(movie.Title, movie.Plot, movie.Genre)} 
                         key={movie._id} 
                         movie={movie}
                         imdbID = {movie.imdbID} 
                         />
 
-                    {showPopup ?
-                        <PopUp
-                            Title={movie.Title}
-                            closePopup={() => setShowPopup(!showPopup)}
-                            Plot={movie.plot}
-                            key={movie._id}
-                        />
-                    : null
-                    }
+                    
                 </div>
         
                 
@@ -43,9 +58,25 @@ function MovieIterator(props){
 
 
     return(
-        <div className={"movies"}>
-            {displayMovies}
+        <div className="contrainer">
+            <Modal 
+                show={show} 
+                style={myStyle}
+                containerStyle={containerStyle}
+                >
+                <div className="popup">
+                    <h1>{title}</h1>
+                    <p>Plot:</p>
+                    <p>{plot}</p>
+                    <p>Genre: {genre}</p>
+                    <Button className="button" color="danger" onClick={() => setShow(false)}>Close</Button>
+                </div>
+            </Modal>
+            <div className={"movies"}>
+                {displayMovies}
+            </div>
         </div>
+        
     )
 
 }
