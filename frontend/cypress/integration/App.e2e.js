70df35a6479a82a02611418544295dfd25e1b1f7
Jakob Bleskestad Ødegård <jakobbod@stud.ntnu.no>
2019-10-28 14:43:29 +0100

    Small update to App.e2e.js to work with new db instance, permission from TA

--- frontend/cypress/integration/App.e2e.js
+++ frontend/cypress/integration/App.e2e.js
@@ -5,14 +5,14 @@ describe('App E2E', () => {
     it('should have a header with text', () => {
         cy.get('h1').should('have.text', 'Group 13');
         cy.contains('Database stats').click()
-        cy.get('p#nr').should('have.text', 'Number of movies in database: 100');
+        cy.get('p#nr').should('have.text', 'Number of movies in database: 250');
     });
     it('should display the correct movie followed by opening pop up', () => {
-        cy.get('input').should('have.text', '').click().type('Die Hard with a Vengeance');
+        cy.get('input').should('have.text', '').click().type('Joker');
         cy.get('Button#searchButton').should('have.value', 'SEARCH').click();
-        cy.get("h4").should('have.text', 'Die Hard with a Vengeance');
+        cy.get("h4").should('have.text', 'Joker');
         cy.get('img').should('have.class', 'Poster').click();
-        cy.get('h1#popTitle').should('have.text', 'Die Hard with a Vengeance');
+        cy.get('h3#popTitle').should('have.text', 'Joker');
     })
     it('should filter correctly', () => {
         cy.get('input').click().type('Die');
